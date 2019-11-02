70b86d30b8dde6acffdb8e6467f4b4cf8222b828
marcoking13 <marcokhodr16@gmail.com>
2019-11-01 19:27:35 -0700

    l

--- src/components/landing/showcase.js
+++ src/components/landing/showcase.js
@@ -13,7 +13,7 @@ export default class Showcase extends React.Component {
               <div className="row">
                 <div className="col-4"/>
                 <div className="col-4">
-                  <a href="http://localhost:5000/spotify-login">  <button className=" ui button black turnWhite ml10 mt10 ">Sign in with Spotify</button> </a>
+                  <a href="/spotify-login">  <button className=" ui button black turnWhite ml10 mt10 ">Sign in with Spotify</button> </a>
                 </div>
                 <div className="col-4"/>
               </div>
