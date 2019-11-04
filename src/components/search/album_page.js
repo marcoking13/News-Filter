243e3c9cf9b74597e7b9b9add05eaed478c20b44
marcoking13 <marcokhodr16@gmail.com>
2019-11-04 11:21:41 -0800

    added progress report

--- src/components/search/album_page.js
+++ src/components/search/album_page.js
@@ -47,6 +47,9 @@ class AlbumPage extends  React.Component {
                             audio.play();
 
                           }}/>
+                          <div class="progress">
+                            <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                          </div>
                        </div>
 
                        </div>
