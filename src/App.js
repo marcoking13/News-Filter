7fb0a4fbbd0feabe30f0a2cb311237633b5ae56f
marcoking13 <marcokhodr16@gmail.com>
2019-11-11 20:40:27 -0800

    entered token in database to be acessed by all components

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ class App extends React.Component {
           <div>
             <Route path = "/" exact component = {LandingPage}></Route>
             <Route path = "/home/:accessToken/:email" render={props => <HomePage UpdateToken = {this.UpdateToken}  />} ></Route>
-            <Route path = "/prof/:accessToken/:email" render={props => <ProfilePage UpdateToken = {this.UpdateToken}  />} ></Route>
+            <Route path = "/prof/:accessToken/:email" render={props => <ProfilePage UpdateToken = {this.UpdateToken} token={this.state.token} />} ></Route>
             <Route path = "/sear/:accessToken/:email"  render={(props) => <SearchPage {...props} token={this.state.token} />} />} />
             <Route path = "/search2" render = { (props) => <SearchPage token = {this.state.token}/>}></Route>
           </div>
