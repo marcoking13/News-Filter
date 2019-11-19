f088c62b2c5ab9f99aaa26ea1bc827b351b4e6af
marcoking13 <marcokhodr16@gmail.com>
2019-11-18 17:45:27 -0800

    redesigned the radio page

--- src/components/radio/play_bar.js
+++ src/components/radio/play_bar.js
@@ -2,12 +2,28 @@ import React from "react";
 
 import Play from "./../../images/play.png";
 import Pause from "./../../images/pause.png";
+import Shuffle from "./../../images/random.png";
 
 import BarStill from "./../progress/progress_bar_still.js";
 import Bar from "./../progress/progress_bar.js";
-
+import GreyStar from "./../../images/grey_star.png";
+import YellowStar from "./../../images/yellow_star.png";
 
 class PlayBar extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      fav:false
+    }
+  }
+  renderStar(){
+    if(this.state.fav){
+      return <img src = {YellowStar} className="w50 ml25" />
+    }else{
+      return <img src = {GreyStar} className="w50 ml25" />
+    }
+  }
+
   renderBar(timer){
     if(timer == 0){
       return <BarStill />
@@ -15,35 +31,52 @@ class PlayBar extends React.Component{
       return <Bar />
     }
   }
-  
-  render(){
+
+  renderBar(){
     if(this.props.song.isPlaying){
-      return(
-        <div className="row">
-          <div className="col-1"/>
-          <div className="col-1">
-            <img src = {Pause} className="w100 playerM"/>
-          </div>
-          <div className="col-8">
-            {this.renderBar(this.props.timer)}
-          </div>
-          <div className="col-1"/>
-        </div>
-      )
+      return <Bar />
     }else{
+      return <BarStill />
+    }
+  }
+
+  renderShuffle(){
+    return <img src = {Shuffle} onClick = {()=>{this.props.CallSongs(this.props.song)}}className="w50 ml25" />
+  }
+  renderButton(){
+    if(this.props.song.isPlaying){
+      return <img src = {Pause} className="w50 ml25"/>
+    }else{
+      return <img src = {Play} className="w50 ml25"/>
+    }
+  }
+
+  render(){
+
       return(
-        <div className="row">
-          <div className="col-1"/>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-4"/>
             <div className="col-1">
-              <img src = {Play} className="w100 playerM"/>
+              {this.renderButton()}
             </div>
-            <div className="col-8">
-              <BarStill />
+            <div className="col-1">
+              {this.renderStar()}
             </div>
-          <div className="col-1"/>
+            <div className="col-1">
+              {this.renderShuffle()}
+            </div>
+            <div className="col-4"/>
+          </div>
+          <div className="row">
+              <div className="col-2"/>
+              <div className="col-8">
+                {this.renderBar()}
+              </div>
+          </div>
         </div>
       )
-    }
+
   }
 }
 
--- src/components/radio/song_box.js
+++ src/components/radio/song_box.js
@@ -8,36 +8,33 @@ import "./../../css/radio.css";
 class SongBox extends React.Component{
 
   render(){
-      if(window.innerWidth < 600){
+      if(window.innerWidth > 600){
         return (
-          <div className="row">
+          <div className="row positionUpward">
 
-            <div className="col-3"/>
-            <div className="col-6">
-            <img className="w100 br10px bounce" src = {this.props.song.image} />
-            <h6 className="text-center cw bold">{this.props.song.songName}</h6>
-            <p className="text-center cw"> {this.props.song.artist} </p>
-          </div>
+            <div className="col-4"/>
+            <div className="col-4">
+              <img className="w100 bounce" src = {this.props.song.image} />
+              <h6 className="text-center cw bold">{this.props.song.songName}</h6>
+              <p className="text-center cw"> {this.props.song.artist} </p>
+            </div>
+            <div className="col-4"/>
 
-          <div className="col-2">
-              <img className="w100 shuffle br10px"  onClick  = {()=>{this.props.CallSongs(this.props.songs)}}src = {Arrow} />
-          </div>
+
         </div>
         )
       }else{
         return(
 
-          <div className="row">
-            <div className="col-4"/>
-            <div className="col-4">
-              <img className="w100 br10px" src =  {this.props.song.image} />
+          <div className="row positionUpward">
+            <div className="col-1"/>
+            <div className="col-10">
+              <img className="w100 " src =  {this.props.song.image} />
               <h6 className="text-center cw bold"> {this.props.song.songName} </h6>
               <p className="text-center cw"> {this.props.song.artist} </p>
             </div>
-            <div className="col-2"/>
-            <div className="col-1">
-                <img className="w100 br10px" onClick  = {()=>{this.props.CallSongs(this.props.songs)}} src = {Arrow} />
-            </div>
+
+            <div className="col-1"/>
           </div>
         )
       }
