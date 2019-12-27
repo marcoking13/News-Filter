ea1c46df709e6feb3aaf31801de9bd06ce6974af
marcoking13 <marcokhodr16@gmail.com>
2019-12-26 20:33:41 -0800

    l

--- src/components/search/songs/song_results.js
+++ src/components/search/songs/song_results.js
@@ -186,8 +186,8 @@ class SongResults extends  React.Component {
             <div className="col-3"/>
             <div className="col-6">
               <div className="row">
-                <div className="col-2"/>
-                <div className="col-8">
+                <div className="col-1"/>
+                <div className="col-10">
                   <div className="row">
                   {this.renderSongs()}
                   </div>
