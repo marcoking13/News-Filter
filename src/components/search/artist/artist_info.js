9a7a1d1cc0edeba99123e5258f607564a76585f6
marcoking13 <marcokhodr16@gmail.com>
2019-12-26 16:22:34 -0800

    l

--- src/components/search/artist/artist_info.js
+++ src/components/search/artist/artist_info.js
@@ -5,12 +5,12 @@ import "./../../../css/utility.css";
 class ArtistInfo extends React.Component {
   render(){
     return(
-      <div className="row jumbotron">
-        <div className="col-3"/>
-        <div className="col-4 bb">
+      <div className="row ">
+        <div className="col-2"/>
+        <div className="col-4 jumbotron bb">
           <img alt = "artist"  className="w70 rounded float-left" src= {this.props.artist.images[0].url}/>
         </div>
-        <div className="col-5 bb">
+        <div className="col-4 jumbotron bb">
           <ul className="artDPUL mt5">
               <li className="list-circle mt5  cw">Name:<strong>{this.props.artist.name}</strong></li>
               <li className="list-circle mt5 cw">{"Type: "+this.props.artist.type}</li>
@@ -19,6 +19,7 @@ class ArtistInfo extends React.Component {
               <li className="list-circle mt5 cw">{"Followers: "+this.props.artist.followers.total}</li>
           </ul>
         </div>
+          <div className="col-2"/>
       </div>
     );
   }
